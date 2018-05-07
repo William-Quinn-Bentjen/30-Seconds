73ca4ea8b32f65f1ddbf6e4ac2c9e01cffada0b7
Unknown <s179051@students.aie.edu.au>
2018-05-07 11:09:38 -0700

    worked on resources

---
+++ 30 Seconds/Assets/Quinn/Scripts/Resource/Resource.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resource : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PickUpResource();
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PickUpResource();
+        }
+    }
+    public void PickUpResource()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ResourceHolder.Backpack.Add(this);
+            gameObject.SetActive(false);
+        }
+    }
+}
