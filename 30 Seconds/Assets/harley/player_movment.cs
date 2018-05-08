1f4a46346fe850f02f81ce357bbe318c99685dc0
Unknown <wolfking068@gmail.com>
2018-05-08 09:31:42 -0700

    stuff

--- 30 Seconds/Assets/harley/player_movment.cs
+++ 30 Seconds/Assets/harley/player_movment.cs
@@ -23,10 +23,13 @@ public class player_movment : MonoBehaviour,IDamageable {
         if (Input.GetKey(KeyCode.A))
         {
             transform.position += Vector3.left * speed * Time.deltaTime;
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+            
         }
         if (Input.GetKey(KeyCode.D))
         {
             transform.position += Vector3.right * speed * Time.deltaTime;
+            transform.rotation = Quaternion.Euler(0, -90, 0);
         }
 
     }
